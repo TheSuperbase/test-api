e51c3a3a1f7d668619a64081179718d36f1431a8
전정훈 <jeonjeonghoon@Tube-MacBookPro-14.local>
2025-11-28 02:34:20 +0900

    CICD Test3

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello World!';
+    return 'Hello World! 25.11.28 2:33';
   }
 }
