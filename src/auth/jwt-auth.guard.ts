fc0312888b2cb10f9356f72387e48191a1d6c709
전정훈 <jeonjeonghoon@Tube-MacBookPro-14.local>
2025-12-22 06:28:36 +0900

    관리자 권한 필요 시 에러 처리 표시

--- src/auth/jwt-auth.guard.ts
+++ src/auth/jwt-auth.guard.ts
@@ -1,5 +1,22 @@
-import { Injectable } from '@nestjs/common';
+import { ExecutionContext, Injectable, UnauthorizedException } from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
 
 @Injectable()
-export class JwtAuthGuard extends AuthGuard('jwt') {}
+export class JwtAuthGuard extends AuthGuard('jwt') {
+  handleRequest<TUser = any>(
+    err: any,
+    user: any,
+    _info: any,
+    _context: ExecutionContext,
+  ): TUser {
+    if (err || !user) {
+      throw new UnauthorizedException({
+        statusCode: 401,
+        error: 'Unauthorized',
+        code: 'ADMIN_LOGIN_REQUIRED',
+        message: '관리자 로그인이 필요합니다',
+      });
+    }
+    return user;
+  }
+}
